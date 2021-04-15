5837e4e544b84efe54798377d1f94d429732b49f
Anis KCHAOU <kchaouanis26@gmail.com>
2021-04-15 14:07:45 +0200

    allow ssl auth

--- db/db.js
+++ db/db.js
@@ -6,6 +6,7 @@ const client = new Client({
     database: 'd49alk7j6atcra',
     password: '624f36c102e3b9a941f9d25c34bb7d9c3cfb7178cef7b5f727792f4c5596ae85',
     port: 5432,
+    ssl: true
 });
 
 client.connect();
@@ -14,27 +15,27 @@ const queryCreateDb = "CREATE TABLE posts (id SERIAL PRIMARY KEY,title varchar,p
 
 
 client
-.query(queryCreateDb)
-.then(res => {
-    console.log('Table is successfully created');
-})
-.catch(err => {
-    console.error(err);
-})
-.finally(() => {
-   // initDb()
+    .query(queryCreateDb)
+    .then(res => {
+        console.log('Table is successfully created');
+    })
+    .catch(err => {
+        console.error(err);
+        throw err
+    })
+    .finally(() => {
+        // initDb()
 
-});
+    });
 
 //initDb()
 
-function initDb()
-{
-    queryInsertDb="INSERT INTO posts (title, post,author,image,date)VALUES ('Lorem Ipsum','Duis cursus orci sapien, vel porttitor odio fringilla eget. Donec aliquet nunc dolor, in pulvinar dolor efficitur vel. Cras molestie tortor quis eros elementum, ut convallis ipsum tempor.','imperdiet velit','https://picsum.photos/200/300','2020-08-09'),('mattis quis','Suspendisse tempus magna a tellus vehicula, ac viverra velit mattis. Vivamus tristique euismod diam, non luctus magna. Donec libero dui','George','https://picsum.photos/200/300','2020-06-23'),('Lorem Ipsum','Duis cursus orci sapien, vel porttitor odio fringilla eget. Donec aliquet nunc dolor, in pulvinar dolor efficitur vel. Cras molestie tortor quis eros elementum, ut convallis ipsum tempor.','imperdiet velit','https://picsum.photos/200/300','2020-08-09'),('mattis quis','Suspendisse tempus magna a tellus vehicula, ac viverra velit mattis. Vivamus tristique euismod diam, non luctus magna. Donec libero dui','George','https://picsum.photos/200/300','2020-06-23')";
+function initDb() {
+    queryInsertDb = "INSERT INTO posts (title, post,author,image,date)VALUES ('Lorem Ipsum','Duis cursus orci sapien, vel porttitor odio fringilla eget. Donec aliquet nunc dolor, in pulvinar dolor efficitur vel. Cras molestie tortor quis eros elementum, ut convallis ipsum tempor.','imperdiet velit','https://picsum.photos/200/300','2020-08-09'),('mattis quis','Suspendisse tempus magna a tellus vehicula, ac viverra velit mattis. Vivamus tristique euismod diam, non luctus magna. Donec libero dui','George','https://picsum.photos/200/300','2020-06-23'),('Lorem Ipsum','Duis cursus orci sapien, vel porttitor odio fringilla eget. Donec aliquet nunc dolor, in pulvinar dolor efficitur vel. Cras molestie tortor quis eros elementum, ut convallis ipsum tempor.','imperdiet velit','https://picsum.photos/200/300','2020-08-09'),('mattis quis','Suspendisse tempus magna a tellus vehicula, ac viverra velit mattis. Vivamus tristique euismod diam, non luctus magna. Donec libero dui','George','https://picsum.photos/200/300','2020-06-23')";
     client.query(queryInsertDb).catch(err => {
         console.error(err);
     });
 }
 
 
-module.exports=client;    
+module.exports = client;
--- server.js
+++ server.js
@@ -1,96 +1,96 @@
-const client=require('./db/db.js');
-const express=require('express');
-const path=require('path');
-const cors=require('cors');
-const app=express();
+const client = require('./db/db.js');
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/all",(req,res)=>{
+app.get("/all", (req, res) => {
 
-   client.query('SELECT * from posts order by id desc', (err, result) => {
-      if (err){
-      	console.log(err);
-        res.send({success:false,result:err});
-      }else
-      {
-        res.send({success:true,result:result.rows});
-      }
+  client.query('SELECT * from posts order by id desc', (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send({ success: false, result: err });
+    } else {
+      res.send({ success: true, result: result.rows });
+    }
   });
-});
+})
 
-app.post("/add",(req,res)=>{
+app.post("/add", (req, res) => {
 
-const post={
-	title:req.body.title,
-	post:req.body.post,
-	author:req.body.author,
-	date:req.body.date,
-  image:req.body.image
-};
-console.log(post);
+  const post = {
+    title: req.body.title,
+    post: req.body.post,
+    author: req.body.author,
+    date: req.body.date,
+    image: req.body.image
+  };
+  console.log(post);
 
-client.query("INSERT INTO posts (title, post,author,image,date) VALUES ('"+post.title+"','"+post.post+"','"+post.author+"','"+post.image+"','"+post.date+"')",(err, result) => {
-      if (err){
-        console.log(err);
-        res.send({success:false,result:err});
-      }else
-      {
-        res.send({success:true,result:result});
-      }
+  client.query("INSERT INTO posts (title, post,author,image,date) VALUES ('" + post.title + "','" + post.post + "','" + post.author + "','" + post.image + "','" + post.date + "')", (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send({ success: false, result: err });
+    } else {
+      res.send({ success: true, result: result });
+    }
   });
-    
-});
 
-
-app.post("/edit",(req,res)=>{
-
-const post={
-  id:req.body.id,
-  title:req.body.title,
-  post:req.body.post,
-  author:req.body.author,
-  date:req.body.date,
-  image:req.body.image
-};
-
-
-client.query("UPDATE posts SET image='"+post.image+"', title='"+post.title+"', post='"+post.post+"', author='"+post.author+"',date='"+post.date+"' WHERE id="+post.id,(err, result) => {
-      if (err){
-        console.log(err);
-        res.send({success:false,result:err});
-      }else
-      {
-        res.send({success:true,result:result});
-      }
-  });
-    
 });
 
 
+app.post("/edit", (req, res) => {
 
-app.post("/delete",(req,res)=>{
-const post={
-  id:req.body.id,
-};
+  const post = {
+    id: req.body.id,
+    title: req.body.title,
+    post: req.body.post,
+    author: req.body.author,
+    date: req.body.date,
+    image: req.body.image
+  };
 
 
-client.query("DELETE FROM posts WHERE id="+post.id,(err, result) => {
-      if (err){
-        console.log(err);
-        res.send({success:false,result:err});
-      }else
-      {
-        res.send({success:true,result:result});
-      }
+  client.query("UPDATE posts SET image='" + post.image + "', title='" + post.title + "', post='" + post.post + "', author='" + post.author + "',date='" + post.date + "' WHERE id=" + post.id, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send({ success: false, result: err });
+    } else {
+      res.send({ success: true, result: result });
+    }
   });
-    
-});
+
+})
 
 
-app.listen(process.env.PORT||3000,err=>{
-if(err) console.error(err);
-console.log("server started at : ", process.env.PORT||3000)
+
+app.post("/delete", (req, res) => {
+  const post = {
+    id: req.body.id,
+  }
+
+
+  client.query("DELETE FROM posts WHERE id=" + post.id, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send({ success: false, result: err });
+    } else {
+      res.send({ success: true, result: result });
+    }
+  });
+
+})
+
+
+app.listen(process.env.PORT || 3000, err => {
+  if (err) {
+    console.error(err);
+    throw err
+  }
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+  console.log("server started at : ", process.env.PORT || 3000)
 });
